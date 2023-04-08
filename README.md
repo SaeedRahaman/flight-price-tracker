7c5b3d1c239dc963aa5b4c2f1f0390c9a4c3af64
Saeed <>
2023-04-08 17:07:35 -0400

    feat:enable headless mode

--- README.md
+++ README.md
@@ -2,10 +2,13 @@
 grabs flight prices from airlines
     - only works with jetblue currently
 
+## install chrome and chromedriver into folder
+the chrome app flatpak and the chromedriver of the SAME VERSION is needed in the folder to run.
+
 ## create config.py with email and password configuration
 
 insert into `config.py`
 EMAIL = "email"
 PASSWORD = "password"
 
-if you do not need email notifications, comment out send_email functions
+if you do not need email notifications, comment out send_email function
--- jetblue.py
+++ jetblue.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 from time import sleep
 
@@ -19,15 +18,18 @@ from send_email import send
 from selenium.webdriver.remote.remote_connection import LOGGER
 LOGGER.setLevel(l.WARNING)
 
-def search_flights(l: l.Logger, departure: str, destination: str, depart_date: str, return_date: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
+DEBUG = True
+
+def search_flights(log: l.Logger, departure: str, destination: str, depart_date: str, return_date: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
 
     url = f"https://www.jetblue.com/booking/flights?from={departure}&to={destination}&depart={depart_date}&return={return_date}&isMultiCity=false&noOfRoute=1&lang=en&adults=1&children=0&infants=0&sharedMarket=false&roundTripFaresFlag=false&usePoints=false"
 
     browser_options = Options()
     browser_options.binary_location = './chrome-linux/chrome'
+    browser_options.add_argument("--headless=new")
+    browser_options.add_argument("--window-size=1920,1080")
     s = Service("./chromedriver_linux64/chromedriver")
     driver = webdriver.Chrome(service=s, options=browser_options)
-    driver.set_window_size(1920, 1080)
 
     driver.get(url)
 
@@ -35,12 +37,15 @@ def search_flights(l: l.Logger, departure: str, destination: str, depart_date: s
     WebDriverWait(driver,10).until(EC.frame_to_be_available_and_switch_to_it((By.CLASS_NAME, 'truste_popframe')))
     WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"/html/body/div[8]/div[1]/div/div[3]/a[1]"))).click()
     
-    l.info("getting departing flights")
+    log.info("getting departing flights")
     sleep(20)
 
     try:
         html = driver.page_source
-        write_html(html, "departure.html")
+
+        if DEBUG:
+            write_html(html, "departure.html")
+
         departures, lowest_index = find_flight_info(html, l)
         
         # fix lowest_index for indexing on xpath
@@ -56,24 +61,26 @@ def search_flights(l: l.Logger, departure: str, destination: str, depart_date: s
         WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, fare_xpath))).click()
 
     except Exception as e:
-        l.error(f"could not click on departing flight selection \n{e}")
+        log.error(f"could not click on departing flight selection \n{e}")
         return pd.DataFrame(), pd.DataFrame()
 
-    l.info("getting return flights")
+    log.info("getting return flights")
     sleep(10)
 
     try:
         html = driver.page_source
-        write_html(html, "returns.html")
+
+        if DEBUG:
+            write_html(html, "returns.html")
 
         returns, lowest_index = find_flight_info(html, l)
     
     except Exception as e:
-        l.error(f"could not find return flight information for selected date \n{e}")
+        log.error(f"could not find return flight information for selected date \n{e}")
         return pd.DataFrame(), pd.DataFrame()
 
     driver.quit()
-    l.info("done")
+    log.info("done")
     return departures, returns
 
 
@@ -86,31 +93,46 @@ def save_csv(csv: pd.DataFrame, date: str, filename: str) -> None:
 def flight(depart_code: str, return_code: str, depart_date: str, return_date: str) -> bool:
     # set logging config
     filename = f"output-{depart_date}-{return_date}.log"
-    l.basicConfig(filename=filename, level=l.INFO, filemode="w")
+
+    # create handler for logging to file
+    file_handler = l.FileHandler(filename)
+    file_handler.setLevel(l.INFO)
+
+    # create handler for logging to terminal
+    stream_handler = l.StreamHandler()
+    stream_handler.setLevel(l.INFO)
+
+    # combine handlers to logger
+    log = l.getLogger()
+    log.setLevel(l.INFO)
+    log.addHandler(file_handler)
+    log.addHandler(stream_handler)
 
     MAX_RETRIES = 1
     retries = 0
-    l.info(f"running at time = {datetime.now()}")
-    l.info(f"depart date = {depart_date} and return date = {return_date}")
+    log.info(f"running at time = {datetime.now()}")
+    log.info(f"depart date = {depart_date} and return date = {return_date}")
 
     # retry logic
     while retries < MAX_RETRIES:
 
         # try to get flight data
-        departures, returns = search_flights(l, depart_code, return_code, depart_date, return_date)
+        departures, returns = search_flights(log, depart_code, return_code, depart_date, return_date)
 
         # check that flight data was received
         if len(departures) > 0 and len(returns) > 0:
-            save_csv(departures, depart_date, "test_departures.csv")
-            save_csv(returns, return_date, "test_returns.csv")
-            l.info("data saved")
+
+            if DEBUG:
+                save_csv(departures, depart_date, "test_departures.csv")
+                save_csv(returns, return_date, "test_returns.csv")
+                log.info("data saved")
 
             # l.info("Sending Email...")
             # send(departures, returns, depart_date, return_date, filename)
             return True
         
         else:
-            l.error(f"a dataframe is emtpy. retry #{retries} \n")
+            log.error(f"a dataframe is emtpy. retry #{retries} \n")
             retries += 1
                 
 
